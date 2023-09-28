d6bf47b8828cbb6808e9d6c221fa7bcb650a5019
Hiiro9Homura <granderaart@gmail.com>
2023-09-28 21:04:49 +0600

    Rename and Comment

--- task-app/views.py
+++ task-app/views.py
@@ -4,19 +4,20 @@ from .models import Task
 from .serializers import TaskSerializer
 
 #Create your views here.
-class TaskListView(generics.ListCreateAPIView):
+
+class TaskListView(generics.ListCreateAPIView): #Просмотр записей
    queryset = Task.objects.all()
    serializer_class = TaskSerializer
 
-class TaskCreateView(generics.CreateAPIView):
+class TaskCreateView(generics.CreateAPIView):  #Создание записи
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-class TaskUpdateView(generics.UpdateAPIView):
+class TaskUpdateView(generics.UpdateAPIView):  #Изменение записи по ID
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-class TaskDeleteView(generics.DestroyAPIView):
+class TaskDeleteView(generics.DestroyAPIView):   #Удаление записи по ID
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
